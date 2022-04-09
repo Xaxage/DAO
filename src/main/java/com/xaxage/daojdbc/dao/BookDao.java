c4a2624f6fa7325baa3fa217e87dd1e836a52a8a
xaxage <xaxage@gmail.com>
2022-04-09 15:02:28 +0400

    Add paging to Spring data JPA.

--- src/main/java/com/xaxage/daojdbc/dao/BookDao.java
+++ src/main/java/com/xaxage/daojdbc/dao/BookDao.java
@@ -2,9 +2,20 @@ package com.xaxage.daojdbc.dao;
 
 
 import com.xaxage.daojdbc.domain.Book;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 
 public interface BookDao {
+    List<Book> findAllBooksSortByTitle(Pageable pageable);
+
+    List<Book> findAllBooks(Pageable pageable);
+
+    List<Book> findAllBooks(int pageSize, int offSet);
+
+    List<Book> findAllBooks();
+
     Book getById(Long id);
 
     Book findBookByTitle(String title);
--- src/main/java/com/xaxage/daojdbc/dao/BookDaoImpl.java
+++ src/main/java/com/xaxage/daojdbc/dao/BookDaoImpl.java
@@ -2,18 +2,53 @@ package com.xaxage.daojdbc.dao;
 
 import com.xaxage.daojdbc.domain.Book;
 import com.xaxage.daojdbc.repositories.BookRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 
 @Component
 public class BookDaoImpl implements BookDao {
+
     private final BookRepository bookRepository;
 
     public BookDaoImpl(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public List<Book> findAllBooksSortByTitle(Pageable pageable) {
+        Page<Book> bookPage = bookRepository.findAll(pageable);
+
+        return bookPage.getContent();
+    }
+
+    @Override
+    public List<Book> findAllBooks(Pageable pageable) {
+        return bookRepository.findAll(pageable).getContent();
+    }
+
+    @Override
+    public List<Book> findAllBooks(int pageSize, int offset) {
+        Pageable pageable = PageRequest.ofSize(pageSize);
+
+        if (offset > 0) {
+            pageable = pageable.withPage(offset / pageSize);
+        } else {
+            pageable = pageable.withPage(0);
+        }
+
+        return this.findAllBooks(pageable);
+    }
+
+    @Override
+    public List<Book> findAllBooks() {
+        return bookRepository.findAll();
     }
 
     @Override
@@ -23,8 +58,7 @@ public class BookDaoImpl implements BookDao {
 
     @Override
     public Book findBookByTitle(String title) {
-        return bookRepository.findBookByTitle(title)
-                .orElseThrow(EntityNotFoundException::new);
+        return bookRepository.findBookByTitle(title).orElseThrow(EntityNotFoundException::new);
     }
 
     @Override
@@ -35,11 +69,12 @@ public class BookDaoImpl implements BookDao {
     @Transactional
     @Override
     public Book updateBook(Book book) {
-        Book fetched = bookRepository.getById(book.getId());
-        fetched.setTitle(book.getTitle());
-        fetched.setIsbn(book.getIsbn());
-        fetched.setPublisher(book.getPublisher());
-        return bookRepository.save(fetched);
+        Book foundBook = bookRepository.getById(book.getId());
+        foundBook.setIsbn(book.getIsbn());
+        foundBook.setPublisher(book.getPublisher());
+        foundBook.setAuthorId(book.getAuthorId());
+        foundBook.setTitle(book.getTitle());
+        return bookRepository.save(foundBook);
     }
 
     @Override
